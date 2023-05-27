4aa036abc3af2d9be33c69a13f518318b66ad98d
TeradonGenesis <48543482+TeradonGenesis@users.noreply.github.com>
2023-05-28 04:05:03 +0800

    Add recog model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useWhisper } from '@chengsokdara/use-whisper'
 
-function App() {
+const App = () => {
+  /**
+   * you have more control like this
+   * do whatever you want with the recorded speech
+   * send it to your own custom server
+   * and return the response back to useWhisper
+   */
+  const onTranscribe = async (blob) => {
+    const base64 = await new Promise(
+      (resolve) => {
+        const reader = new FileReader()
+        reader.onloadend = () => resolve(reader.result)
+        reader.readAsDataURL(blob)
+      }
+    )
+    const body = JSON.stringify({ file: base64, model: 'whisper-1' })
+    const headers = { 'Content-Type': 'application/json' }
+    const { default: axios } = await import('axios')
+    const response = await axios.post('/api/whisper', body, {
+      headers,
+    })
+    const { text } = await response.data
+    // you must return result from your server in Transcript format
+    return {
+      blob,
+      text,
+    }
+  }
+
+  const {
+    transcript,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    onTranscribe,
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Transcribed Text: {transcript.text}</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => stopRecording()}>Stop</button>
     </div>
-  );
+  )
 }
 
 export default App;
