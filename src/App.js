d3e39ecc7a8aaa4a9d16f5de1fcdae439f2700e9
clifford96 <cliffordlee260996@gmail.com>
2023-05-28 07:30:33 +0800

    improve design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useWhisper } from '@chengsokdara/use-whisper'
+import { useState } from 'react';
 
 const App = () => {
   /**
@@ -7,6 +8,9 @@ const App = () => {
    * send it to your own custom server
    * and return the response back to useWhisper
    */
+
+  const [transText, setTransTest] = useState('')
+
   const onTranscribe = async (blob) => {
     const base64 = await new Promise(
       (resolve) => {
@@ -22,6 +26,7 @@ const App = () => {
       headers,
     })
     const { text } = await response.data
+    setTransTest(text)
     // you must return result from your server in Transcript format
     const data = await axios.post('http://127.0.0.1:5000/api/v1/payagent/', {query: text})
     console.log(data)
@@ -40,12 +45,44 @@ const App = () => {
   })
 
   return (
-    <div>
-      <p>Transcribed Text: {transcript.text}</p>
-      <button onClick={() => startRecording()}>Start</button>
-      <button onClick={() => stopRecording()}>Stop</button>
+    <div style={{ textAlign: "center" }}>
+      <p style={{ fontSize: "24px", color: "#333", marginBottom: "20px" }}>
+        Transcribed Text: {transText}
+      </p>
+      <div>
+        <button
+          style={{
+            backgroundColor: "#4CAF50",
+            color: "white",
+            fontFamily: "Open Sans, Helvetica, sans-serif",
+            fontSize: "16px",
+            padding: "10px 20px",
+            border: "none",
+            cursor: "pointer",
+            marginRight: "10px",
+          }}
+          onClick={() => startRecording()}
+        >
+          Start
+        </button>
+        <button
+          style={{
+            backgroundColor: "#4CAF50",
+            color: "white",
+            fontFamily: "Open Sans, Helvetica, sans-serif",
+            fontSize: "16px",
+            padding: "10px 20px",
+            border: "none",
+            cursor: "pointer",
+          }}
+          onClick={() => stopRecording()}
+        >
+          Stop
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+  
+};
 
 export default App;
